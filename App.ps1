30e217a1c17a6d97fe24b2fdaf18dcf9d55bbf36
Glendeschema <lehlohonologlen@gmail.com>
2018-09-17 16:59:17 +0200

    Created Firewall Rule

--- App.ps1
+++ App.ps1
@@ -4,7 +4,12 @@ $username = Get-Credential
 $Cred =New-Object System.Management.Automation.PSCredential($username.UserName , $username.Password)
 Login-AzureRmAccount -Credential $Cred
 
-
+<#
+    1. Enter Resource Group Name and SQL Server Name
+    2.Validates if RG Exists , if not it will create a new one and re-initilize variable
+    3.Get public IP of this current host
+    4.Create a Firewall Rule to allow this machine to use Azure SQL
+#>
 $RGName = read-host "Enter RGName"
 $SQLServerName = Read-host "Enter SQL Server Name"
 
@@ -14,22 +19,19 @@ if(!$RGName){
 }
 $RG = Get-AzureRmResourceGroup | Where ResourceGroupName -eq $RGName
 
-$DBServer = Get-AzureRmSqlServer -ResourceGroupName $RGName -ServerName "$SQLServerName.database.windows.net"         
+$DBServer = Get-AzureRmSqlServer -ResourceGroupName $RG.ResourceGroupName   
+
+$DBName = Get-AzureRmSqlDatabase -ResourceGroupName $RG.ResourceGroupName -ServerName $DBServer.ServerName
 
 #Getting Public Address 
 $publicIp = (Invoke-WebRequest http://myexternalip.com/raw -UseBasicParsing).Content -replace "`n"
 
-New-azurermsql
+New-AzureRmSqlServerFirewallRule -FirewallRuleName "Allow IP" `
+-StartIpAddress $publicIp -EndIpAddress $publicIp `
+-ServerName $DBServer.ServerName `
+-ResourceGroupName $RGName -Verbose
 
-$cred = New-Object System.Management.Automation.
+$cred = New-Object System.Management.Automation.PSCredential("Glen",(Get-Credential).Password)
 
 
-$params = @{
-	'Database' = 'rctsql'
-	'ServerInstance' = 'rctdemo.database.windows.net'
-	'Username' = 'Glen'
-	'Password' = 'Morolong1993'
-	'Query' = 'SELECT * FROM salesLT.EmployeeName'
-}
-Invoke-Sqlcmd @params
 
